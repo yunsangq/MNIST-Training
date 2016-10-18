17abdb2f5fb35f091e59bdb2d5b8bffeb21c2b45
yunsangq <yunsangq@gmail.com>
2016-10-18 11:44:33 +0900

    SGD MLP 완성

--- MNIST_Loader.py
+++ MNIST_Loader.py
@@ -2,7 +2,7 @@ import os
 import struct
 from array import array
 import numpy as np
-
+import json
 
 class DataLoader:
     def __init__(self, path='.'):
@@ -14,24 +14,14 @@ class DataLoader:
         self.train_img_fname = 'train-images-idx3-ubyte'
         self.train_lbl_fname = 'train-labels-idx1-ubyte'
 
-        self.test_images = []
-        self.test_labels = []
-
-        self.train_images = []
-        self.train_labels = []
-
     def LoadData(self):
-        test_ims, test_labels = self.load(os.path.join(self.path, self.test_img_fname),
+        test_data = self.test_load(os.path.join(self.path, self.test_img_fname),
                                 os.path.join(self.path, self.test_lbl_fname))
 
-        train_ims, train_labels = self.load(os.path.join(self.path, self.train_img_fname),
+        train_data = self.train_load(os.path.join(self.path, self.train_img_fname),
                                 os.path.join(self.path, self.train_lbl_fname))
 
-        self.test_images = test_ims
-        self.test_labels = test_labels
-
-        self.train_images = train_ims
-        self.train_labels = train_labels
+        return train_data, test_data
 
     # Vectorise Int into (10, 1) vector representation, with 1.0 at jth positon and 0.0 elsewhere
     def VectorResult(self, j):
@@ -39,7 +29,7 @@ class DataLoader:
         vect[j] = 1.0
         return vect
 
-    def load(self, path_img, path_lbl):
+    def train_load(self, path_img, path_lbl):
         with open(path_lbl, 'rb') as file:
             magic, size = struct.unpack(">II", file.read(8))
             if magic != 2049:
@@ -63,10 +53,49 @@ class DataLoader:
         for i in range(size):
             images[i][:] = image_data[i * rows * cols:(i + 1) * rows * cols]
 
+        print 'TRAIN Data Normalization'
+        for i in xrange(size):
+            for j in xrange(784):
+                images[i][j] /= 256.0
+
         result_images = [np.reshape(x, (784, 1)) for x in images]
         result_labels = [self.VectorResult(y) for y in labels]
 
         data = zip(result_images, result_labels)
+        return data
+
+    def test_load(self, path_img, path_lbl):
+        with open(path_lbl, 'rb') as file:
+            magic, size = struct.unpack(">II", file.read(8))
+            if magic != 2049:
+                raise ValueError('Magic number mismatch, expected 2049,'
+                                 'got {}'.format(magic))
+
+            labels = array("B", file.read())
+
+        with open(path_img, 'rb') as file:
+            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
+            if magic != 2051:
+                raise ValueError('Magic number mismatch, expected 2051,'
+                                 'got {}'.format(magic))
+
+            image_data = array("B", file.read())
+
+        images = []
+        for i in range(size):
+            images.append([0] * rows * cols)
+
+        for i in range(size):
+            images[i][:] = image_data[i * rows * cols:(i + 1) * rows * cols]
+
+        print 'TEST Data Normalization'
+        for i in xrange(size):
+            for j in xrange(784):
+                images[i][j] /= 256.0
+
+        result_images = [np.reshape(x, (784, 1)) for x in images]
+
+        data = zip(result_images, labels)
         return data
 
 
--- mlp.py
+++ mlp.py
@@ -1,245 +1,123 @@
 import random
 import numpy as np
-# import matplotlib.pyplot as plt
-import MNIST
+import MNIST_Loader
+import matplotlib.pyplot as plt
 import time
 
-LEARNING_RATE = 3.0
-EPOCH = 50
-TOTAL_DATA_SIZE = 0
-TRAINING_DATA_SIZE = 0
-TEST_DATA_SIZE = 0
-MINI_BATCH_SIZE = 10
-x1 = []
-y1 = []
 
-f_x1 = open("./x1.txt", 'w')
-f_y1 = open("./y1.txt", 'w')
-f_weight_input = open("./weight_input.txt", 'w')
-f_weight_output = open("./weight_output.txt", 'w')
+def disp(epochs, hist):
+    fig = plt.figure(facecolor='white')
+    fig.canvas.set_window_title('Accuracy per Epoch')
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(range(epochs), hist, color='#1F77B4')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Accuracy')
+    plt.show()
 
-
-def sigmoid(x):
-    return 1.0/(1.0+np.exp(-1 * x))
-
-
-def sigmoid_prime(y):
-    return y * (1.0 - y)
-
-
-class MLP(object):
-    def __init__(self, n_input, n_hidden, n_output):
-        # + 1 bias
-        self.n_input = n_input + 1
-        self.n_hidden = n_hidden
-        self.n_output = n_output
-
-        self.act_input = [1.0] * self.n_input
-        self.act_hidden = [1.0] * self.n_hidden
-        self.act_output = [1.0] * self.n_output
-
-        self.weight_input = np.zeros((len(self.act_input), len(self.act_hidden)))
-        self.weight_output = np.zeros((len(self.act_hidden), len(self.act_output)))
-
-        self.change_weight_input = np.zeros((len(self.act_input), len(self.act_hidden)))
-        self.change_weight_output = np.zeros((len(self.act_hidden), len(self.act_output)))
-
-        for i in range(self.n_input):
-            for j in range(self.n_hidden):
-                self.weight_input[i][j] = random.uniform(-0.05, 0.05)
-
-        for i in range(self.n_hidden):
-            for j in range(self.n_output):
-                self.weight_output[i][j] = random.uniform(-0.05, 0.05)
-
-    # RGB 0~255 (x - min) / (max - min)
-    def normalize_data(self, x):
-        return (x - 0.0) / (255.0 - 0.0)
+class MLP:
+    def __init__(self):
+        self.biases = [np.random.randn(y, 1) for y in NN[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(NN[:-1], NN[1:])]
+        self.epoch_accuracy = []
 
     def feed_forward(self, inputs):
-        for i in range(self.n_input - 1):
-            self.act_input[i] = self.normalize_data(inputs[i])
-            # self.act_input[i] = inputs[i]
+        for b, w in zip(self.biases, self.weights):
+            inputs = self.sigmoid(np.dot(w, inputs) + b)
+        return inputs
 
-        for j in range(self.n_hidden):
-            _sum = 0.0
-            for i in range(self.n_input):
-                _sum += self.act_input[i] * self.weight_input[i][j]
-            self.act_hidden[j] = sigmoid(_sum)
+    def train(self, x, y):
+        delta_b = [np.zeros(b.shape) for b in self.biases]
+        delta_w = [np.zeros(w.shape) for w in self.weights]
 
-        for k in range(self.n_output):
-            _sum = 0.0
-            for j in range(self.n_hidden):
-                _sum += self.act_hidden[j] * self.weight_output[j][k]
-            self.act_output[k] = sigmoid(_sum)
-        return self.act_output[:]
+        # Feed Forward
+        activation = x
+        activations = [x]
 
-    def back_propagate(self, targets):
-        output_deltas = [0.0] * self.n_output
-        for k in range(self.n_output):
-            error = targets[k] - self.act_output[k]
-            output_deltas[k] = sigmoid_prime(self.act_output[k]) * error
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            activation = self.sigmoid(z)
+            activations.append(activation)
 
-        hidden_deltas = [0.0] * self.n_hidden
-        for j in range(self.n_hidden):
-            error = 0.0
-            for k in range(self.n_output):
-                error += output_deltas[k] * self.weight_output[j][k]
-            hidden_deltas[j] = sigmoid_prime(self.act_hidden[j]) * error
+        # Backpropagation
+        output_delta = self.cost(activations[-1], y) * self.dsigmoid(activations[-1])
+        delta_b[-1] = output_delta
+        delta_w[-1] = np.dot(output_delta, activations[-2].transpose())
 
-        for j in range(self.n_hidden):
-            for k in range(self.n_output):
-                # mini-Batch
-                self.change_weight_output[j][k] += output_deltas[k] * self.act_hidden[j]
-                # Single-step
-                # self.weight_output[j][k] += LEARNING_RATE * output_deltas[k] * self.act_hidden[j]
+        for i in xrange(2, 3):
+            hidden_delta = np.dot(self.weights[-i+1].transpose(), output_delta) * self.dsigmoid(activations[-i])
+            delta_b[-i] = hidden_delta
+            delta_w[-i] = np.dot(hidden_delta, activations[-i-1].transpose())
+        return delta_b, delta_w
 
-        for i in range(self.n_input):
-            for j in range(self.n_hidden):
-                # mini-Batch
-                self.change_weight_input[i][j] += hidden_deltas[j] * self.act_input[i]
-                # Single-step
-                # self.weight_input[i][j] += LEARNING_RATE * hidden_deltas[j] * self.act_input[i]
+    def update(self, mini_batch, learn_rate):
+        batch_b = [np.zeros(b.shape) for b in self.biases]
+        batch_w = [np.zeros(w.shape) for w in self.weights]
 
-        error = np.array(targets) - np.array(self.act_output)
-        error = np.linalg.norm(error)
-        error = 0.5 * error ** 2
-        return error
+        for x, y in mini_batch:
+            delta_batch_b, delta_batch_w = self.train(x, y)
+            batch_b = [b + delta_b for b, delta_b in zip(batch_b, delta_batch_b)]
+            batch_w = [w + delta_w for w, delta_w in zip(batch_w, delta_batch_w)]
 
-    def update(self):
-        for j in range(self.n_hidden):
-            for k in range(self.n_output):
-                self.weight_output[j][k] += LEARNING_RATE / MINI_BATCH_SIZE * (self.change_weight_output[j][k] / TRAINING_DATA_SIZE)
+        self.weights = [w - (learn_rate / len(mini_batch)) * bw for w, bw in zip(self.weights, batch_w)]
+        self.biases = [b - (learn_rate / len(mini_batch)) * bb for b, bb in zip(self.biases, batch_b)]
 
-        for i in range(self.n_input):
-            for j in range(self.n_hidden):
-                self.weight_input[i][j] += LEARNING_RATE / MINI_BATCH_SIZE * (self.change_weight_input[i][j] / TRAINING_DATA_SIZE)
-
-        self.change_weight_output = np.zeros((len(self.act_hidden), len(self.act_output)))
-        self.change_weight_input = np.zeros((len(self.act_input), len(self.act_hidden)))
-
-    def training(self, patterns, outputs):
-        for i in range(EPOCH):
-            error = 0.0
-            mini_batch_idx = np.random.randint(TOTAL_DATA_SIZE, size=TRAINING_DATA_SIZE)
-            m_patterns = []
-            m_outputs = []
-            for idx in mini_batch_idx:
-                m_patterns.append(patterns[idx])
-                m_outputs.append(outputs[idx])
+    def stochastic(self, train_data, epochs, mini_batch_size, learn_rate, test_data):
+        n_test = len(test_data)
+        n = len(train_data)
+        for i in xrange(epochs):
             start_time = time.time()
-            for j in range(TRAINING_DATA_SIZE):
-                inputs = m_patterns[j]
-                targets = m_outputs[j]
-                self.feed_forward(inputs)
-                error += self.back_propagate(targets)
-                if j % 1000 == 0 and j != 0:
-                    tmp = error / float(j)
-                    print ('time -> ' + str(j))
-                    print ('error -> %-.5f' % tmp)
+            random.shuffle(train_data)
+            mini_batches = [train_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+
+            for batch in mini_batches:
+                self.update(batch, learn_rate)
+
+            accuracy = (float(self.test(test_data)) / float(n_test)) * 100
+            self.epoch_accuracy.append(accuracy)
+            print 'Epoch : {0}'.format(i)
+            print 'Accuracy : {0:.2f}%'.format(accuracy)
             print 'Epoch Running Time : %.02f' % (time.time() - start_time)
-            error /= float(TRAINING_DATA_SIZE)
-            self.update()
-            x1.append(i)
-            y1.append(error)
-            print ('epoch -> ' + str(i))
-            print ('epoch total error -> %-.5f' % error)
+        return self.epoch_accuracy
 
-    def test(self, patterns, labels):
-        error = 0.0
-        correct = 0.0
-        for i in range(TEST_DATA_SIZE):
-            output = self.feed_forward(patterns[i])
-            tmp = np.array(labels[i]) - np.array(self.act_output)
-            tmp = np.linalg.norm(tmp)
-            error += 0.5 * tmp ** 2
-            t = np.array(labels[i]).argmax()
-            o = np.array(output).argmax()
-            if t == o:
-                correct += 1.0
-            # print(t, '->', o)
-            # print (patterns[i], output)
-        error /= float(TEST_DATA_SIZE)
-        print ('TEST error : %-.5f' % error)
-        acc = correct / TEST_DATA_SIZE * 100
-        print ('Accuracy : ' + str(acc))
+    def sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
 
-        for j in range(self.n_hidden):
-            for k in range(self.n_output):
-                f_weight_output_data = "%f," % self.weight_output[j][k]
-                f_weight_output.write(f_weight_output_data)
-            f_weight_output_data = "\n"
-            f_weight_output.write(f_weight_output_data)
+    def dsigmoid(self, z):
+        return z * (1.0 - z)
 
-        for i in range(self.n_input):
-            for j in range(self.n_hidden):
-                f_weight_input_data = "%f," % self.weight_input[i][j]
-                f_weight_input.write(f_weight_input_data)
-            f_weight_input_data = "\n"
-            f_weight_input.write(f_weight_input_data)
+    def cost(self, output_activations, y):
+        return output_activations - y
+
+    def test(self, test_data):
+        results = [(np.argmax(self.feed_forward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in results)
+
 
 if __name__ == '__main__':
-    mn = MNIST.MNIST()
-    img, label = mn.load_training()
-    TOTAL_DATA_SIZE = len(img)
-    TRAINING_DATA_SIZE =  TOTAL_DATA_SIZE / MINI_BATCH_SIZE
-    # TRAINING_DATA_SIZE = 10
-    test_img, test_label = mn.load_testing()
-    TEST_DATA_SIZE = len(test_img)
+    NN = [784, 60, 10]
+    LEARN_RATE = 0.5
+    EPOCHS = 50
+    MINI_BATCH_SIZE = 10
+    TRAIN_DATA = None
+    TEST_DATA = None
 
-    NN = MLP(784, 60, 10)
+    print 'MNIST Data Loading...'
+    data_loader = MNIST_Loader.DataLoader()
+    TRAIN_DATA, TEST_DATA = data_loader.LoadData()
+    print 'MNIST Data Loaded!!'
 
+    mlp = MLP()
     total_start_time = time.time()
-    NN.training(img, label)
+    epoch_accuracy = mlp.stochastic(
+        TRAIN_DATA,
+        EPOCHS,
+        MINI_BATCH_SIZE,
+        LEARN_RATE,
+        TEST_DATA
+    )
+    print 'Training Finished'
     print 'Training Running Time : %.02f' % (time.time() - total_start_time)
+    disp(EPOCHS, epoch_accuracy)
 
-    NN.test(test_img, test_label)
 
-    for x in x1:
-        x1_data = "%d\n" % x
-        f_x1.write(x1_data)
 
-    for y in y1:
-        y1_data = "%f\n" % y
-        f_y1.write(y1_data)
-
-    f_x1.close()
-    f_y1.close()
-    f_weight_input.close()
-    f_weight_output.close()
-"""
-    plt.plot(x1, y1)
-    plt.xlabel('epoch')
-    plt.ylabel('cost')
-    plt.show()
-"""
-"""
-   img = [
-       [0, 0],
-       [0, 1],
-       [1, 0],
-       [1, 1]
-   ]
-   label = [
-       [0],
-       [1],
-       [1],
-       [0]
-   ]
-   test_img = [
-       [0, 0],
-       [0, 1],
-       [1, 0],
-       [1, 1]
-   ]
-   test_label = [
-       [0],
-       [1],
-       [1],
-       [0]
-   ]
-   TRAINING_DATA_SIZE = len(img)
-   TEST_DATA_SIZE = len(test_img)
-
-   NN = MLP(2, 4, 1)
-   """
