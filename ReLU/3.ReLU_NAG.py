8ed303aa36ce09a16a397f35e2603d41488ecd5d
yunsangq <yunsangq@gmail.com>
2016-10-21 14:51:20 +0900

    ReLU적용 및 폴더구조 변경 재학습중.. 에폭 100으로 증가

---
+++ ReLU/3.ReLU_NAG.py
@@ -0,0 +1,237 @@
+import random
+import numpy as np
+import MNIST_Loader
+import matplotlib.pyplot as plt
+import time
+import json
+
+
+def err_disp(epochs, train, valid):
+    fig = plt.figure(facecolor='white')
+    fig.canvas.set_window_title('ReLU_NAG_Cost per Epoch')
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(range(epochs), train, color='#1F77B4', label='Training')
+    ax.plot(range(epochs), valid, color='#b41f1f', label='Validation')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Cost')
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.show()
+
+
+def acc_disp(epochs, train, valid):
+    fig = plt.figure(facecolor='white')
+    fig.canvas.set_window_title('ReLU_NAG_Accuracy per Epoch')
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(range(epochs), train, color='#1F77B4', label='Training')
+    ax.plot(range(epochs), valid, color='#b41f1f', label='Validation')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Accuracy')
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.show()
+
+
+class MLP:
+    def __init__(self):
+        self.biases = [np.random.randn(y, 1) for y in NN[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(NN[:-1], NN[1:])]
+        self.train_acc = []
+        self.valid_acc = []
+        self.train_cost = []
+        self.valid_cost = []
+        self.before_b = [np.zeros(b.shape) for b in self.biases]
+        self.before_w = [np.zeros(w.shape) for w in self.weights]
+        self.prev_vb = [np.zeros(b.shape) for b in self.biases]
+        self.prev_vw = [np.zeros(w.shape) for w in self.weights]
+
+    def feed_forward(self, inputs):
+        relu_check = 0
+        for b, w in zip(self.biases, self.weights):
+            if relu_check == 0:
+                inputs = self.ReLU(np.dot(w, inputs) + b)
+                relu_check = 1
+            else:
+                inputs = self.sigmoid(np.dot(w, inputs) + b)
+        return inputs
+
+    def train(self, x, y):
+        delta_b = [np.zeros(b.shape) for b in self.biases]
+        delta_w = [np.zeros(w.shape) for w in self.weights]
+
+        # Feed Forward
+        activation = x
+        activations = [x]
+
+        relu_check = 0
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            if relu_check == 0:
+                activation = self.ReLU(z)
+                relu_check = 1
+            else:
+                activation = self.sigmoid(z)
+            activations.append(activation)
+
+        # Backpropagation
+        # outdelta
+        delta = self._error(activations[-1], y) * self.dReLU(activations[-1])
+        delta_b[-1] = delta
+        delta_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for i in xrange(2, 3):
+            # hiddendelta
+            delta = np.dot(self.weights[-i+1].transpose(), delta) * self.dReLU(activations[-i])
+            delta_b[-i] = delta
+            delta_w[-i] = np.dot(delta, activations[-i-1].transpose())
+        return delta_b, delta_w
+
+    def update(self, mini_batch, learn_rate, momentum):
+        batch_b = [np.zeros(b.shape) for b in self.biases]
+        batch_w = [np.zeros(w.shape) for w in self.weights]
+
+        for x, y in mini_batch:
+            delta_batch_b, delta_batch_w = self.train(x, y)
+            batch_b = [b + delta_b for b, delta_b in zip(batch_b, delta_batch_b)]
+            batch_w = [w + delta_w for w, delta_w in zip(batch_w, delta_batch_w)]
+
+        """
+        vprev = v
+        v = -lr*dx + mu*v
+        x += -mu*-lr*dx + mu*vprev + (1+mu)*-lr*dx + mu*v
+        """
+        self.prev_vw = self.before_w
+        self.before_w = [-(learn_rate / len(mini_batch)) * bw + momentum * bew
+                         for bw, bew in zip(batch_w, self.before_w)]
+        self.weights = [w - momentum * (-(learn_rate / len(mini_batch)) * bw + momentum * pvw)
+                        + (1 + momentum) * (-(learn_rate / len(mini_batch)) * bw + momentum * bew)
+                        for w, bw, bew, pvw in zip(self.weights, batch_w, self.before_w, self.prev_vw)]
+
+        self.prev_vb = self.before_b
+        self.before_b = [-(learn_rate / len(mini_batch)) * bb + momentum * beb
+                         for bb, beb in zip(batch_b, self.before_b)]
+        self.biases = [b - momentum * (-(learn_rate / len(mini_batch)) * bb + momentum * pvb)
+                       + (1 + momentum) * (-(learn_rate / len(mini_batch)) * bb + momentum * beb)
+                       for b, bb, beb, pvb in zip(self.biases, batch_b, self.before_b, self.prev_vb)]
+
+    def stochastic(self, train_data, epochs, mini_batch_size, learning_rate, valid_data, test_data, momentum):
+        n_test = len(test_data)
+        n_valid = len(valid_data)
+        n = len(train_data)
+        for i in xrange(epochs):
+            start_time = time.time()
+            random.shuffle(train_data)
+            mini_batches = [train_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+
+            for batch in mini_batches:
+                self.update(batch, learning_rate, momentum)
+
+            self.train_cost.append(self.get_train_cost(train_data))
+            self.valid_cost.append(self.get_cost(valid_data))
+            self.train_acc.append((float(self.train_test(train_data)) / float(n)) * 100)
+            self.valid_acc.append((float(self.test(valid_data)) / float(n_valid)) * 100)
+            accuracy = (float(self.test(valid_data)) / float(n_valid)) * 100
+            print 'Epoch : {0}'.format(i)
+            print 'Accuracy : {0:.2f}%'.format(accuracy)
+            """
+            print 'Cost : {0:.2f}'.format(error)
+            print 'Accuracy : {0:.2f}%'.format(accuracy)
+            """
+            print 'Epoch Running Time : %.02f' % (time.time() - start_time)
+
+        error = self.get_cost(test_data)
+        accuracy = (float(self.test(test_data)) / float(n_test)) * 100
+        print 'Test Data Cost : {0:.2f}'.format(error)
+        print 'Test Data Accuracy : {0:.2f}%'.format(accuracy)
+
+        self.save("ReLU_NAG.json", accuracy)
+
+        return self.train_acc, self.valid_acc, self.train_cost, self.valid_cost
+
+    def save(self, filename, accuracy):
+        data = {"train_cost": self.train_cost,
+                "valid_cost": self.valid_cost,
+                "train_accuracy": self.train_acc,
+                "valid_accuracy": self.valid_acc,
+                "test_accuracy": accuracy,
+                "weights": [w.tolist() for w in self.weights],
+                "biases": [b.tolist() for b in self.biases]}
+        f = open(filename, "w")
+        json.dump(data, f)
+        f.close()
+
+    def sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def ReLU(self, x):
+        return x * (x > 0)
+
+    def dReLU(self, x):
+        return 1. * (x > 0)
+
+    def _error(self, output_activations, y):
+        return output_activations - y
+
+    def train_test(self, test_data):
+        results = [(np.argmax(self.feed_forward(x)), np.argmax(y)) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in results)
+
+    def test(self, test_data):
+        results = [(np.argmax(self.feed_forward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in results)
+
+    def get_train_cost(self, test_data):
+        results = 0.0
+        for (x, y) in test_data:
+            results += 0.5 * np.linalg.norm(self._error(self.feed_forward(x), y)) ** 2
+        results /= float(len(test_data))
+        return results
+
+    def get_cost(self, test_data):
+        results = 0.0
+        for (x, y) in test_data:
+            result_labels = self.vectorresult(y)
+            results += 0.5 * np.linalg.norm(self._error(self.feed_forward(x), result_labels)) ** 2
+        results /= float(len(test_data))
+        return results
+
+    def vectorresult(self, j):
+        vect = np.zeros((10, 1))
+        vect[j] = 1.0
+        return vect
+
+
+if __name__ == '__main__':
+    NN = [784, 60, 10]
+    LEARNING_RATE = 0.1
+    EPOCHS = 100
+    MINI_BATCH_SIZE = 10
+    TRAIN_DATA = None
+    VALID_DATA = None
+    TEST_DATA = None
+    MOMENTUM = 0.5
+
+    print 'MNIST Data Loading...'
+    data_loader = MNIST_Loader.DataLoader()
+    TRAIN_DATA, VALID_DATA, TEST_DATA = data_loader.loaddata()
+    print 'MNIST Data Loaded!!!!'
+
+    mlp = MLP()
+    total_start_time = time.time()
+    train_acc, valid_acc, train_cost, valid_cost = mlp.stochastic(
+        TRAIN_DATA,
+        EPOCHS,
+        MINI_BATCH_SIZE,
+        LEARNING_RATE,
+        VALID_DATA,
+        TEST_DATA,
+        MOMENTUM
+    )
+    print 'Training Finished'
+    print 'Training Running Time : %.02f' % (time.time() - total_start_time)
+    acc_disp(EPOCHS, train_acc, valid_acc)
+    err_disp(EPOCHS, train_cost, valid_cost)
+
+
